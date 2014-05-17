a28fab3743f5b5149925543a2013856394213d84
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2014-05-16 22:17:12 -0700

    First pages commit

---
+++ app/app.js
@@ -0,0 +1,64 @@
+//Global variable to restart animation
+
+function restart() {
+    $(this).find('li:last').after($(this).find('li:first'));
+    $(this).css({marginLeft:0});
+}
+
+
+//When document loads this will execute
+$(document).ready(function() {
+
+  function mottsDefault(){
+    $('.second').mouseenter(function(){
+      $('.frame-1').fadeOut(800);
+      $('.action').fadeIn(1000, 'linear');
+      $('.banner').fadeIn(1000);
+    });
+
+ }
+
+mottsDefault();
+
+//this is where the magic happens - carousel motion
+function carouselSpin(){
+  setInterval(function(){
+      $('.fancy').fadeToggle(900, 'linear').animate({marginLeft: '-186px'}, 900, restart).fadeIn(900);
+    }, 4000);
+}
+
+carouselSpin();
+restart();
+
+
+//This is to make arrows work - need to work on right arrow
+$(document).on('click', '.left-arrow', function(){
+  $('.fancy').fadeToggle(900, 'linear').stop(true, false).animate({marginLeft: '-186px'}, 900, restart).fadeIn(900).stop(true, true).fadeToggle(900, 'linear').stop(true, false).animate({marginLeft: '-186px'}, 900, restart).fadeIn(900).stop(true, true);
+    });
+});
+
+$(document).on('click', '.right-arrow', function(){
+  $('.fancy').fadeToggle(900, 'linear').stop(true, false).animate({marginLeft: '-186px'}, 900, restart).fadeIn(900).stop(true, true).fadeToggle(900, 'linear').stop(true, false).animate({marginLeft: '-186px'}, 900, restart).fadeIn(900).stop(true, true);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
